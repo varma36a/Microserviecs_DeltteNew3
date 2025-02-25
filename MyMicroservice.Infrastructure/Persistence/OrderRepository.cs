44ca743741930e1d63d52587c80c8389b27e3f3d
Rohith <varma36a@gmail.com>
2025-02-25 21:40:13 +0530

    gitignore changes

---
+++ MyMicroservice.Infrastructure/Persistence/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyMicroservice.Domain.Entities;
+using MyMicroservice.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace MyMicroservice.Infrastructure.Persistence;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Order order)
+    {
+        await _context.Orders.AddAsync(order);
+        await _context.SaveChangesAsync();
+    }
+}
